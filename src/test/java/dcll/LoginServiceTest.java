471ee1d1e2e486546790c8c13a3fba46e7789ee2
Youyou <yousraelghzizal@hotmail.fr>
2016-04-15 14:53:51 +0200

    tests passees

--- src/test/java/dcll/LoginServiceTest.java
+++ src/test/java/dcll/LoginServiceTest.java
@@ -1,34 +1,57 @@
 package dcll;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 /**
- * Unit test for simple LoginService.
+ * Created by HP on 15/04/2016.
  */
-public class LoginServiceTest
-        extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public LoginServiceTest(String testName) {
-        super(testName);
+public class LoginServiceTest {
+    //ArrayList<String> loginsExistants=new ArrayList<String>();
+    //String[] logins={"YELG"};
+
+   // LoginService lg=new LoginService(logins);
+   // this.loginsExistants = new ArrayList<String>(Arrays.asList());
+
+    @Test
+    public void testLoginExists() throws Exception {
+        String[] logins={"YELG"};
+
+        LoginService lg=new LoginService(logins);
+       assertTrue(lg.loginExists("YELG"));
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(LoginServiceTest.class);
+    @Test
+    public void testAddLogin() throws Exception {
+             String[]logins={"YELG"};
+             LoginService lg=new LoginService(logins);
+             lg.addLogin("DUPS");
+             ArrayList<String> l=new ArrayList<String>();
+             l.add("YELG");
+             l.add("DUPS");
+             assertEquals(l,lg.findAllLogins());
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp() {
-        assertTrue(true);
+    @Test
+    public void testFindAllLoginsStartingWith() throws Exception {
+        String[]logins={"YELG"};
+        LoginService lg=new LoginService(logins);
+        ArrayList<String> l=new ArrayList<String>();
+        l.add("YELG");
+        assertEquals(l,lg.findAllLoginsStartingWith("Y"));
+    }
+
+    @Test
+    public void testFindAllLogins() throws Exception {
+        String[]logins={"YELG"};
+        LoginService lg=new LoginService(logins);
+        ArrayList<String> l=new ArrayList<String>();
+        l.add("YELG");
+        assertEquals(l,lg.findAllLogins());
     }
 }
